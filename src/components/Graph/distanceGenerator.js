b09819431190d396f109116fe2a5e4f7eca07b30
micaellagouveia <micaella2212@gmail.com>
2022-04-24 13:46:07 -0300

    Add djikstra into generateDistances

--- src/components/Graph/distanceGenerator.js
+++ src/components/Graph/distanceGenerator.js
@@ -9,6 +9,75 @@ const setDistance = (graph, source, node, distance) => {
     graph.get(node).find(x => x.link === source).distance = distance;
 }
 
+// Djikstra para gerar tabela de distâncias para o ponto inicial
+
+const createPriorityQueue = () => {
+    return [];
+}
+
+const enqueue = (pqueue, element, price) => {
+    let exists = false;
+
+    for (let i = 0; i < pqueue.length; i++) {
+        if (pqueue[i].price > price) {
+            pqueue.splice(i, 0, { element, price })
+            exists = true;
+            break;
+        }
+    }
+
+    if (!exists) pqueue.push({ element, price })
+
+}
+
+const dequeue = (pqueue) => {
+    if (!pqueue.length) throw "Is Empty!"
+
+    return pqueue.shift()
+}
+
+const djikstraAlgorithm = (graph, startNode, endNode) => {
+    let prices = {};
+    let prevNode = {};
+    const queue = createPriorityQueue();
+    const startNodes = graph.get(startNode);
+
+    prices[startNode] = 0;
+    enqueue(queue, startNode, 0);
+
+    const vertices = graph.keys();
+    for (let vert of vertices) {
+        if (vert !== startNode) prices[vert] = Infinity;
+        prevNode[vert] = null;
+    }
+
+    while (queue.length) {
+        let minNode = dequeue(queue);
+        let currNode = minNode.element;
+
+        const nodes = graph.get(currNode);
+
+        if (!startNodes.length) return "Sem caminho utilizando esse nó inicial";
+
+        for (let node of nodes) {
+            const sumPrice = prices[currNode] + node.distance;
+
+            if (sumPrice < prices[node.link]) {
+                prevNode[node.link] = currNode;
+                prices[node.link] = sumPrice;
+                enqueue(queue, node.link, prices[node.link])
+            }
+        }
+
+    }
+    if (prices[endNode] === Infinity) return "Não é possível formar caminho até o nó final"
+    else {
+        return prices[endNode];
+    }
+
+}
+
+
 const generateDistances = (graph, qtd) => {
     // gera as distâncias aleatórias para os nós já pré definidos
     setDistance(graph, 0, 1, randomDistance(1, qtd));
@@ -30,6 +99,13 @@ const generateDistances = (graph, qtd) => {
     setDistance(graph, 10, 11, randomDistance(1, qtd));
     setDistance(graph, 12, 11, randomDistance(1, qtd));
     setDistance(graph, 12, 2, randomDistance(1, qtd));
+
+    const distances = [];
+    for (let i = 1; i <= 12; i++) {
+        const distance = djikstraAlgorithm(graph, 0, i);
+        distances.push(distance);
+    }
+    return distances;
 }
 
 export default generateDistances;
